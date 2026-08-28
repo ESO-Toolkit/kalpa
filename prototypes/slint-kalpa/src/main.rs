4bc454afaa7e3a1b84f29c266a450653b3e6cec1
braydenpb72 <braydenpb72@gmail.com>
2026-08-28 14:31:32 -0700

    test(slint): complete addon detail fixtures

--- prototypes/slint-kalpa/src/main.rs
+++ prototypes/slint-kalpa/src/main.rs
@@ -23635,6 +23635,8 @@ CombatMetrics_SavedVariables = {
                 created: "08/05/14".to_string(),
                 screenshots: Vec::new(),
                 download_url: "https://cdn.esoui.com/downloads/file1360.zip".to_string(),
+                change_log: String::new(),
+                archived_versions: Vec::new(),
             },
         );
 
@@ -23668,6 +23670,8 @@ CombatMetrics_SavedVariables = {
             created: "08/05/14".to_string(),
             screenshots: Vec::new(),
             download_url: "https://cdn.esoui.com/downloads/file1360.zip".to_string(),
+            change_log: String::new(),
+            archived_versions: Vec::new(),
         };
 
         let installed =
